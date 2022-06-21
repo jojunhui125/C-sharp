40ad9c8f5db2381da79f75e7ac3930df51ee9c58
junhee <86397805+jojunhui125@users.noreply.github.com>
2022-06-21 16:07:51 +0900

    aekJoon2562.cs
    aekJoon2562.cs

---
+++ BaekJoon2562.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace program
+{
+    class Program
+    {
+        private static int[] arrn;
+        static void Main(string[] args)
+        {
+            arrn = new int[9];
+            int maxi = arrn[0];
+            int count = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                arrn[i] = int.Parse(Console.ReadLine());
+
+                if (arrn[i] > maxi)
+                {
+                    maxi = arrn[i];
+                    count = i + 1;
+                }
+
+            }
+
+            Console.WriteLine("{0}", maxi);
+            Console.WriteLine("{0}", count);
+
+
+        }
+    }
+}
+
