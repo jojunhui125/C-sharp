8d5d4f5887e693059c87b1e5cd6bea7acf401188
junhee <86397805+jojunhui125@users.noreply.github.com>
2022-06-21 15:31:01 +0900

    BaekJoon1330.cs
    BaekJoon1330.cs

---
+++ BaekJoon1330.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            
+            if(int.Parse(input[0]) < int.Parse(input[1]))
+            {
+                Console.WriteLine("<");
+            }
+            else if (int.Parse(input[0]) > int.Parse(input[1]))
+            {
+                Console.WriteLine(">");
+            }
+            else
+            {
+                Console.WriteLine("==");
+            }
+        }
+    }
+}
+
