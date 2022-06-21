3e2f79f409c677df2d50a9349186e4a142fe406a
junhee <86397805+jojunhui125@users.noreply.github.com>
2022-06-21 16:08:04 +0900

    BaekJoon2675.cs
    BaekJoon2675.cs

---
+++ BaekJoon2675.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace program
+{
+    class Program
+    {
+        
+      
+        static void Main(string[] args)
+        {
+            string answer = "";
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i=0;i<n;i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                int number = int.Parse(input[0]);
+
+                for(int j=0;j<input[1].Length;j++)
+                {
+                    for(int k=0;k<number;k++)
+                    {
+                        answer += input[1][j];
+                    }
+                }
+
+                Console.WriteLine("{0}", answer);
+                answer = "";
+
+            }
+
+
+        }
+    }
+}
+
