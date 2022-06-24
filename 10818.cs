e744ad2f943a0c7bd497fb907e68f796c7a5f274
junhee <86397805+jojunhui125@users.noreply.github.com>
2022-06-24 16:45:55 +0900

    10818

---
+++ 10818.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            string inputs = Console.ReadLine();
+            string[] input = inputs.Split();
+            int[] nums = Array.ConvertAll(input, int.Parse);
+            int maxi = nums[0];
+            int mini = nums[0];
+
+            for (int i = 0; i < num; i++)
+            {
+                if (nums[i] < mini)
+                {
+                    mini = nums[i];
+                }
+                else if (nums[i] > maxi)
+                {
+                    maxi = nums[i];
+                }
+            }
+
+            Console.WriteLine(mini + " " + maxi);
+
+        }
+    }
+}
