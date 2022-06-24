7acfea293416c29986bd330af59b67befc86a7a2
junhee <86397805+jojunhui125@users.noreply.github.com>
2022-06-24 16:07:33 +0900

    8958

---
+++ 8958.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < number; i++)
+            {
+                string input = Console.ReadLine();
+                int cnt = 0;
+                int plus = 1;
+                
+
+                for(int j=0; j < input.Length; j++)
+                {
+                    if(input[j] == 'O')
+                    {
+                        cnt += plus;
+                        plus++;
+                    }
+                    else
+                    {
+                        plus = 1;
+                    }
+                }
+                Console.WriteLine(cnt);
+            }
+
+
+            
+
+            
+
+
+        }
+    }
+}
