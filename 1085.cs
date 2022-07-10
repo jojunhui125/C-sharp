5dd0ee4f4abba46a33212c08d81e5f3b95b8ff35
junhee <86397805+jojunhui125@users.noreply.github.com>
2022-07-10 18:56:15 +0900

    1085

---
+++ 1085.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] num = new int[4];
+
+            num[0] = a[3] - a[1];
+            num[1] = a[1];
+            num[2] = a[0];
+            num[3] = a[2] - a[0];
+
+            Console.WriteLine(num.Min());
+
+
+
+
+        }
+    }
+}
