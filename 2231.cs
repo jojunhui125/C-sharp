70f4d4a2cd5204191c8d76c376974f101d586937
junhee <86397805+jojunhui125@users.noreply.github.com>
2022-07-10 19:52:29 +0900

    2231
    변수 sum, 소문자로 선언  x

---
+++ 2231.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int Sum = 0;
+            int result = 0;
+
+            for(int i=0;i<num;i++)
+            {
+                char[] charr = i.ToString().ToCharArray();
+
+                for(int j=0;j<charr.Length;j++)
+                {
+                    Sum += int.Parse(charr[j].ToString());
+                }
+                result = Sum + i;
+
+                if(result==num)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+                else if(i==num-1 && result!=num)
+                {
+                    Console.WriteLine(0);
+                    break;
+                }
+                Sum = 0;
+                
+            }
+
+            
+
+           
+
+
+
+
+        }
+    }
+}
