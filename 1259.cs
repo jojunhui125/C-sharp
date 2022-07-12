e7638719af88fcb1b969887c0b7f7d8b484ac2bb
junhee <86397805+jojunhui125@users.noreply.github.com>
2022-07-12 17:35:55 +0900

    1259

---
+++ 1259.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string a = "";
+                int input = int.Parse(Console.ReadLine());
+
+                if(input!=0)
+                {
+                    for(int j=input.ToString().Length-1; j>=0; j--)
+                    {
+                        a += input.ToString()[j];
+                    }
+                    if(a==input.ToString())
+                    {
+                        Console.WriteLine("yes");
+                    }
+                    else
+                    {
+                        WriteLine("no");
+                    }
+                }
+                
+                else if(input==0)
+                {
+                    break;
+                }
+                
+            }
+        }
+
+
+
+
+
+
+
+
+    }
+
+}
