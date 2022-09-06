d583e901db9cb8512c6359dec639a258517e6132
junhee <86397805+jojunhui125@users.noreply.github.com>
2022-09-06 14:39:13 +0900

    1541

---
+++ 1541.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using static System.Console;
+using System.Threading;
+using System.IO;
+
+namespace project
+{
+	
+
+	public class Program
+	{
+		
+		public static void Main(string[] args)
+		{
+			string input = ReadLine();
+			string[] number = input.Split('-');
+			int n = 0;
+			
+
+
+			
+			foreach(string strnumber in number[0].Split('+'))
+			{
+				n += int.Parse(strnumber);
+			}
+
+			if(number.Length==0)
+			{
+				WriteLine(n);
+			}
+			else
+			{
+				for (int i = 1; i < number.Length; i++)
+				{
+					string[] newinput = number[i].Split('+');
+
+					foreach (string num in newinput)
+					{
+						n -= int.Parse(num);
+					}
+				}
+
+			}
+			
+			WriteLine(n);
+			
+		}
+
+	
+		
+
+
+	}
+
+}
