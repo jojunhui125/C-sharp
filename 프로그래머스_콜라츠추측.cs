920b637e9d9067c5533d1ffe23a60c5743908339
junhee <86397805+jojunhui125@users.noreply.github.com>
2022-09-08 14:07:19 +0900

    콜라츠 추측

---
+++ 프로그래머스_콜라츠추측.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+	public int solution(int num)
+	{
+		long number = num;
+		int answer = 0;
+
+		while (number > 1)
+		{
+			if (number % 2 == 0)
+			{
+				number = number / 2;
+			}
+			else
+			{
+				number = number * 3 + 1;
+			}
+			answer++;
+		}
+		if (answer > 500)
+		{
+			return -1;
+		}
+		else
+		{
+			return answer;
+		}
+
+
+
+
+	}
+}
