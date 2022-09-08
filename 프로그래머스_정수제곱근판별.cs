68a67b71b8dd5a0d05f7e212a10dd30b60d00351
junhee <86397805+jojunhui125@users.noreply.github.com>
2022-09-08 11:47:03 +0900

    프로그래머스_정수제곱근판별

---
+++ 프로그래머스_정수제곱근판별.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Solution
+{
+	public long solution(long n)
+	{
+		long answer = 0;
+
+		for (long i = 1; i <= n; i++)
+		{
+			if (i * i == n)
+			{
+				answer = (i + 1) * (i + 1);
+				break;
+			}
+			else if (i * i >= n)
+			{
+
+				break;
+			}
+
+		}
+		if (answer == 0)
+		{
+			return -1;
+		}
+		return answer;
+
+	}
+}
