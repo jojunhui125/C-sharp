a2ab1e44fad848ed838ffcdc5adff352c7078f17
junhee <86397805+jojunhui125@users.noreply.github.com>
2022-09-08 13:51:42 +0900

    프로그래머스_x만큼 간격이 있는 n개의 숫자

---
+++ 프로그래머스_x만큼 간격이 있는 n개의 숫자.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+	public long[] solution(int x, int n)
+	{
+		List<long> answer = new List<long>();
+
+
+		for (var i = 1; i <= n; i++)
+		{
+			answer.Add(i * x);
+		}
+		return answer.ToArray();
+	}
+}
