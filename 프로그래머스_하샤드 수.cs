1593702565b5b2b029d95fe276c5f77c230ddec6
junhee <86397805+jojunhui125@users.noreply.github.com>
2022-09-08 11:59:25 +0900

    프로그래머스_하샤드 수

---
+++ 프로그래머스_하샤드 수.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Solution
+{
+	public bool solution(int x)
+	{
+		bool answer = true;
+		int sum = 0;
+		int num = x;
+		while (x > 0)
+		{
+			sum += x % 10;
+			x = x / 10;
+		}
+		if (num % sum == 0)
+		{
+			return answer;
+		}
+		else answer = false;
+
+
+		return answer;
+	}
+}
